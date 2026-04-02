7777c50d64815213e5eb0b997f67aa777f4e416f
Tessa Holder <tessajholder@gmail.com>
2026-04-02 12:57:47 -0700

    Update popup.js
    Fixed popup names

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,8 +1,8 @@
 chrome.storage.local.get(['riskData'], (result) => {
     if (result.riskData) {
         // Find the elements in popup.html and update them
-        document.querySelector('.popup-container p').innerText = `Risk Score: ${result.riskData.score}/100`;
-        document.querySelector('.popup-two p:first-child').innerText = `Phishing tactic: ${result.riskData.tactic}`;
-        document.querySelector('.popup-three p').innerText = `Recommendation: ${result.riskData.recommendation}`;
+        document.querySelector('popup-container').innerText = `Risk Score: ${result.riskData.score}/100`;
+        document.querySelector('popup-two').innerText = `Phishing tactic: ${result.riskData.tactic}`;
+        document.querySelector('popup-three').innerText = `Recommendation: ${result.riskData.recommendation}`;
     }
 });
